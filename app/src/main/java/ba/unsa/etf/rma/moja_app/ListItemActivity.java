44c09a7a4e25b8467a4f422f49f45326391a191a
ekarahodza1 <ekarahodza1@etf.unsa.ba>
2020-03-30 00:22:22 +0200

    poceto brisanje

--- app/src/main/java/ba/unsa/etf/rma/moja_app/ListItemActivity.java
+++ app/src/main/java/ba/unsa/etf/rma/moja_app/ListItemActivity.java
@@ -1,25 +1,39 @@
 package ba.unsa.etf.rma.moja_app;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class ListItemActivity extends AppCompatActivity {
 
-    ImageView image;
-    EditText title;
-    EditText amount;
-    EditText date;
-    EditText endDate;
-    EditText description;
-    EditText interval;
-    EditText type;
+    private ImageView image;
+    private EditText title;
+    private EditText amount;
+    private EditText date;
+    private EditText endDate;
+    private EditText description;
+    private EditText interval;
+    private EditText type;
+    private Button delete;
+    private Button OK;
+    private Type type_;
+    private LocalDate date1;
+    private LocalDate date2;
 
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,28 +47,37 @@ public class ListItemActivity extends AppCompatActivity {
         description = findViewById(R.id.editDescription);
         interval = findViewById(R.id.editInterval);
         type = findViewById(R.id.editType);
+        delete = (Button)findViewById(R.id.buttonDelete);
+        OK = (Button)findViewById(R.id.buttonOK);
+
 
         String imageType = getIntent().getStringExtra("type");
+
         if (imageType.matches("INDIVIDUALPAYMENT")) {
             image.setImageResource(R.drawable.individual_payment);
             type.setText("INDIVIDUAL PAYMENT");
+            type_ = Type.INDIVIDUALPAYMENT;
         }
         if (imageType.matches("REGULARPAYMENT")) {
             image.setImageResource(R.drawable.regular_payment);
             type.setText("REGULAR PAYMENT");
+            type_ = Type.REGULARPAYMENT;
 
         }
         if (imageType.matches("PURCHASE")) {
             image.setImageResource(R.drawable.purchase);
             type.setText("PURCHASE");
+            type_ = Type.PURCHASE;
         }
         if (imageType.matches("INDIVIDUALINCOME")) {
             image.setImageResource(R.drawable.individual_income);
             type.setText("INDIVIDUAL INCOME");
+            type_ = Type.INDIVIDUALINCOME;
         }
         if (imageType.matches("REGULARINCOME")) {
             image.setImageResource(R.drawable.regular_income);
             type.setText("REGULAR INCOME");
+            type_ = Type.REGULARINCOME;
         }
 
         title.setText(getIntent().getStringExtra("title"));
@@ -66,6 +89,22 @@ public class ListItemActivity extends AppCompatActivity {
         description.setText(getIntent().getStringExtra("description"));
         date.setText(getIntent().getStringExtra("date"));
         endDate.setText(getIntent().getStringExtra("eDate"));
+
+
+
+        if (!getIntent().getStringExtra("date").matches("")) date1 = LocalDate.parse(getIntent().getStringExtra("date"));
+        if (!getIntent().getStringExtra("eDate").matches("")) date2 = LocalDate.parse(getIntent().getStringExtra("eDate"));
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Transaction t = new Transaction(date1, getIntent().getStringExtra("title"), b.getDouble("amount"),
+                        type_, getIntent().getStringExtra("description"), b.getInt("interval"), date2);
+
+                finish();
+            }
+
+        });
 
 
     }
