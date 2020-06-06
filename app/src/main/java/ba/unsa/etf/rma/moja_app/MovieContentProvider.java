d55d7653887760cbedfcb95ccc933a4a39ebad98
ekarahodza1 <ekarahodza1@etf.unsa.ba>
2020-06-07 00:51:45 +0200

    metode u movie provideru

---
+++ app/src/main/java/ba/unsa/etf/rma/moja_app/MovieContentProvider.java
@@ -0,0 +1,110 @@
+package ba.unsa.etf.rma.moja_app;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MovieContentProvider extends ContentProvider {
+
+    private static final int ALLROWS =1;
+    private static final int ONEROW = 2;
+    private static final UriMatcher uM;
+
+    static {
+        uM = new UriMatcher(UriMatcher.NO_MATCH);
+        uM.addURI("rma.provider.transactions","elements",ALLROWS);
+        uM.addURI("rma.provider.transactions","elements/#",ONEROW);
+    }
+
+    FinanceDBOpenHelper mHelper;
+
+    @Override
+    public boolean onCreate() {
+        mHelper = new FinanceDBOpenHelper(getContext(),
+                FinanceDBOpenHelper.DATABASE_NAME,null,
+                FinanceDBOpenHelper.DATABASE_VERSION);
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        SQLiteDatabase database;
+        try{
+            database=mHelper.getWritableDatabase();
+        }catch (SQLiteException e){
+            database=mHelper.getReadableDatabase();
+        }
+        String groupby=null;
+        String having=null;
+        SQLiteQueryBuilder squery = new SQLiteQueryBuilder();
+
+        switch (uM.match(uri)){
+            case ONEROW:
+                String idRow = uri.getPathSegments().get(1);
+                squery.appendWhere(FinanceDBOpenHelper.INTERNAL_ID+"="+idRow);
+            default:break;
+        }
+        squery.setTables(FinanceDBOpenHelper.TRANSACTION_TABLE);
+        Cursor cursor = squery.query(database,projection,selection,selectionArgs,groupby,having,sortOrder);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        switch (uM.match(uri)){
+            case ALLROWS:
+                return "vnd.android.cursor.dir/vnd.rma.elemental";
+            case ONEROW:
+                return "vnd.android.cursor.item/vnd.rma.elemental";
+            default:
+                throw new IllegalArgumentException("Unsuported uri: "+uri.toString());
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        SQLiteDatabase database;
+        try{
+            database=mHelper.getWritableDatabase();
+        }catch (SQLiteException e){
+            database=mHelper.getReadableDatabase();
+        }
+        long id = database.insert(FinanceDBOpenHelper.TRANSACTION_TABLE, null, values);
+        return uri.buildUpon().appendPath(String.valueOf(id)).build();
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        SQLiteDatabase database;
+        try{
+            database=mHelper.getWritableDatabase();
+        }catch (SQLiteException e){
+            database=mHelper.getReadableDatabase();
+        }
+        int id = database.delete(FinanceDBOpenHelper.TRANSACTION_TABLE, selection, selectionArgs);
+        return id;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        SQLiteDatabase database;
+        try{
+            database=mHelper.getWritableDatabase();
+        }catch (SQLiteException e){
+            database=mHelper.getReadableDatabase();
+        }
+        int id = database.update(FinanceDBOpenHelper.TRANSACTION_TABLE, values, selection, selectionArgs);
+        return id;
+    }
+}
