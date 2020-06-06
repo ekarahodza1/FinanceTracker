902f5f6270ef1b63532cba38875d50fc6dcea6c2
ekarahodza1 <ekarahodza1@etf.unsa.ba>
2020-06-06 11:37:49 +0200

    popravke u helperu

--- app/src/main/java/ba/unsa/etf/rma/moja_app/FinanceDBOpenHelper.java
+++ app/src/main/java/ba/unsa/etf/rma/moja_app/FinanceDBOpenHelper.java
@@ -19,8 +19,10 @@ public class FinanceDBOpenHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public static final String ADD_TABLE = "temporary_A";
+    public static final String TRANSACTION_TABLE = "transactions";
     public static final String TRANSACTION_TITLE = "title";
+    public static final String ID = "id";
+    public static final String INTERNAL_ID = "_id";
     public static final String TRANSACTION_DATE = "date";
     public static final String TRANSACTION_END_DATE = "endDate";
     public static final String TRANSACTION_DESCRIPTION = "itemDescription";
@@ -28,7 +30,8 @@ public class FinanceDBOpenHelper extends SQLiteOpenHelper {
     public static final String TYPE_ID = "TransactionTypeId";
     public static final String TRANSACTION_AMOUNT = "amount";
     private static final String ADD_TABLE_CREATE =
-            "CREATE TABLE IF NOT EXISTS " + ADD_TABLE + " ("
+            "CREATE TABLE IF NOT EXISTS " + TRANSACTION_TABLE + " (" + INTERNAL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + ID + " INTEGER UNIQUE, "
                     + TRANSACTION_TITLE + " TEXT NOT NULL, "
                     + TRANSACTION_DATE + " TEXT, "
                     + TRANSACTION_END_DATE + " TEXT, "
@@ -37,50 +40,20 @@ public class FinanceDBOpenHelper extends SQLiteOpenHelper {
                     + TRANSACTION_AMOUNT + " REAL, "
                     + TYPE_ID + " INTEGER);";
 
-    private static final String ADD_DROP = "DROP TABLE IF EXISTS " + ADD_TABLE;
 
-    public static final String UPDATE_TABLE = "temporary_U";
-    public static final String ID = "id";
-    public static final String TITLE = "title";
-    public static final String DATE = "date";
-    public static final String END_DATE = "endDate";
-    public static final String DESCRIPTION = "itemDescription";
-    public static final String INTERVAL = "transactionInterval";
-    public static final String T_ID = "TransactionTypeId";
-    public static final String AMOUNT = "amount";
-    private static final String UPDATE_TABLE_CREATE =
-            "CREATE TABLE IF NOT EXISTS " + UPDATE_TABLE + " ("
-                    + ID + " INTEGER, "
-                    + TITLE + " TEXT NOT NULL, "
-                    + DATE + " TEXT, "
-                    + END_DATE + " TEXT, "
-                    + DESCRIPTION + " TEXT, "
-                    + INTERVAL + " INTEGER, "
-                    + AMOUNT + " REAL, "
-                    + T_ID + " INTEGER);";
+    private static final String TRANSACTIONS_DROP = "DROP TABLE IF EXISTS " + TRANSACTION_TABLE;
 
-    private static final String UPDATE_DROP = "DROP TABLE IF EXISTS " + UPDATE_TABLE;
 
-    public static final String DELETE_TABLE = "temporary_D";
-    public static final String TRANSACTION_ID = "id";
-    private static final String DELETE_TABLE_CREATE =
-            "CREATE TABLE IF NOT EXISTS " + DELETE_TABLE + " ("
-                    + TRANSACTION_ID + " INTEGER);";
-
-    private static final String DELETE_DROP = "DROP TABLE IF EXISTS " + DELETE_TABLE;
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(ADD_TABLE_CREATE);
-        db.execSQL(UPDATE_TABLE_CREATE);
-        db.execSQL(DELETE_TABLE_CREATE);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(ADD_DROP);
-        db.execSQL(UPDATE_DROP);
-        db.execSQL(DELETE_DROP);
+        db.execSQL(TRANSACTIONS_DROP);
         onCreate(db);
     }
 }
